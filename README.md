78f8e4505c4a82d770587851c47141079c17416d
tone4hook <antone4hook@gmail.com>
2024-04-24 18:03:27 +0900

    Chore: add readme and env flags

---
+++ README.md
@@ -0,0 +1,39 @@
+# Vista Dictionary
+
+Vista Dictionary micro-frontend for a Webpack Module Federation demo. Demonstrates micro-frontends and dictionary functionality.
+
+[View as remote module](https://tone4hook.github.io/vista-dictionary/)
+
+[View host app](https://tone4hook.github.io/vista-currency/)
+
+## Scripts
+
+- `build`: Builds the application for production.
+- `build:dev`: Builds the application for development.
+- `start`: Starts the development server and opens the application in a web browser.
+- `start:live`: Starts the development server with live reloading and opens the application in a web browser.
+- `lint`: Lints the codebase.
+- `lint:fix`: Lints the codebase and automatically fixes any fixable issues.
+- `format`: Formats the codebase using Prettier.
+
+## Author
+
+- Anthony Hook
+
+## License
+
+This project is licensed under the MIT License.
+
+## Dependencies
+
+This project uses several dependencies, including:
+
+- Vue.js for building user interfaces.
+- Vue Router for routing.
+- Pinia for state management.
+- Bootstrap for styling.
+- Babel and TypeScript for transpiling and type checking.
+- ESLint and Prettier for linting and formatting.
+- Webpack for bundling and module federation.
+
+For a full list of dependencies, refer to the `package.json` file.
--- webpack.config.js
+++ webpack.config.js
@@ -4,61 +4,68 @@ const { VueLoaderPlugin } = require('vue-loader');
 const Dotenv = require('dotenv-webpack');
 const packageJson = require('./package.json');
 
-module.exports = () => ({
-  output: {
-    publicPath: 'https://tone4hook.github.io/vista-dictionary/',
-  },
+module.exports = () => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const publicPath = isProduction
+    ? 'https://tone4hook.github.io/vista-dictionary/'
+    : 'http://localhost:8082/';
 
-  resolve: {
-    extensions: ['.tsx', '.ts', '.vue', '.jsx', '.js', '.json'],
-  },
+  return {
+    output: {
+      publicPath,
+    },
 
-  devServer: {
-    port: 8082,
-    historyApiFallback: true,
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.vue', '.jsx', '.js', '.json'],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
-      {
-        test: /\.tsx?$/,
-        use: [
-          'babel-loader',
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              appendTsSuffixTo: ['\\.vue$'],
-              happyPackMode: true,
+    devServer: {
+      port: 8082,
+      historyApiFallback: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.tsx?$/,
+          use: [
+            'babel-loader',
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+                appendTsSuffixTo: ['\\.vue$'],
+                happyPackMode: true,
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-      },
-    ],
-  }, // Add a comma here
+          ],
+        },
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+      ],
+    },
 
-  plugins: [
-    new VueLoaderPlugin(),
-    new ModuleFederationPlugin({
-      name: 'vista_dictionary',
-      filename: 'remoteEntry.js',
-      remotes: {},
-      exposes: {
-        './Vista': './src/pages/Vista.vue',
-      },
-      shared: packageJson.dependencies,
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-    }),
-    new Dotenv(),
-  ],
-});
+    plugins: [
+      new VueLoaderPlugin(),
+      new ModuleFederationPlugin({
+        name: 'vista_dictionary',
+        filename: 'remoteEntry.js',
+        remotes: {},
+        exposes: {
+          './Vista': './src/pages/Vista.vue',
+        },
+        shared: packageJson.dependencies,
+      }),
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+      }),
+      new Dotenv(),
+    ],
+  };
+};
